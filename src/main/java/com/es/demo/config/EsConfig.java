4ec27f9539a7737589aef9f6c21e066bc684a5c4
taomee517 <taomee517@qq.com>
2020-03-20 17:07:57 +0800

    调整配置，新增日志

--- src/main/java/com/es/demo/config/EsConfig.java
+++ src/main/java/com/es/demo/config/EsConfig.java
@@ -16,13 +16,13 @@ public class EsConfig {
     public TransportClient client() throws UnknownHostException {
 
         //es集群连接
-        TransportAddress node = new  TransportAddress(InetAddress.getByName("localhost"),9300);
+        TransportAddress node = new  TransportAddress(InetAddress.getByName("127.0.0.1"),9300);
 //        TransportAddress node1 = new  TransportAddress(InetAddress.getByName("localhost"),8300);
 //        TransportAddress node2 = new  TransportAddress(InetAddress.getByName("localhost"),7300);
 
         //es集群配置（自定义配置） 连接自己安装的集群名称
         Settings settings = Settings.builder()
-//                .put("cluster.name","es-cluster")
+                .put("cluster.name","es-cluster")
                 .build();
 
         PreBuiltTransportClient client = new PreBuiltTransportClient(settings);
