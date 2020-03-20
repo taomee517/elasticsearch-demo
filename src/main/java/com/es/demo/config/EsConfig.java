0b9173039e1ff24ac9656af17c98690aa186e793
Administrator <taomee517@gmail.com>
2020-03-20 23:29:48 +0800

    修改es配置和依赖，版本代差不能太大

--- src/main/java/com/es/demo/config/EsConfig.java
+++ src/main/java/com/es/demo/config/EsConfig.java
@@ -15,8 +15,9 @@ public class EsConfig {
     @Bean
     public TransportClient client() throws UnknownHostException {
 
-        //es集群连接
-        TransportAddress node = new  TransportAddress(InetAddress.getByName("127.0.0.1"),9300);
+        //es集群连接 需要在ElasticSearch各个节点的elasticsearch.yml配置transport.tcp.port
+        //引入的elasticsearch-transport版本不能与实际安装版本相差太大
+        TransportAddress node = new  TransportAddress(InetAddress.getByName("hadoop2"),9300);
 //        TransportAddress node1 = new  TransportAddress(InetAddress.getByName("localhost"),8300);
 //        TransportAddress node2 = new  TransportAddress(InetAddress.getByName("localhost"),7300);
 
