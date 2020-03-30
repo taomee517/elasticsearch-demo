9801d15d16ff005ffa07989ba8ab0ccbe8b890a9
Administrator <taomee517@gmail.com>
2020-03-30 21:43:15 +0800

    日志中打印服务器ip

--- src/main/java/com/es/demo/ElasticsearchDemoApplication.java
+++ src/main/java/com/es/demo/ElasticsearchDemoApplication.java
@@ -1,14 +1,24 @@
 package com.es.demo;
 
+import com.es.demo.utils.IpAddrUtil;
+import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.net.SocketException;
 
 @SpringBootApplication(scanBasePackages={"com.es.demo.*"})
 @EnableSwagger2
 public class ElasticsearchDemoApplication {
 
     public static void main(String[] args) {
+        try {
+            String localIp = IpAddrUtil.getIpAddress().getHostAddress();
+            System.setProperty("local-ip", localIp);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
         SpringApplication.run(ElasticsearchDemoApplication.class, args);
     }
 }
